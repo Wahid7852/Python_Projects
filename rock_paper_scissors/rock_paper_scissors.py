c31acc772ce982341fc39de60695271b9d33c10e
ArzooCreates01 <119441983+ArzooCreates01@users.noreply.github.com>
2023-04-28 14:00:21 +0530

    Added some comments.

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -1,30 +1,37 @@
 import random
 
+
 def play():
     player = input("Choose 'r' for Rock, 'p' for Paper and 's' for seciorrs\n")
     computer = random.choice(['r','p','s'])
-
+    
+    # When draw occurs
     if player==computer:
         print("\nMatch Draw")
 
+    # When player chooses rock
     elif (player=='r'):
         if computer=='p':
             print("\nComputer WINS")
         else:
             print("\nPlayer WINS")
 
+    # When player chooses paper
     elif player=='p':
         if computer=='r':
             print("\nPlayer WINS")
         else:
             print("\nComputer WINS")
 
+    # When player chooses scissor
     elif player=='s':
         if computer=='r':
             print("\nComputer WINS")
         else:
             print("\nPlayer WINS")
+            
     print(f"You have choosen {player} and the computer has choosen {computer} ")
 
+    
 play()
 
